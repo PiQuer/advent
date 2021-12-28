4d790436a57a8b4e335104ce5337976b49e5520f
Raimar Sandner <mail@raimar-sandner.de>
2021-12-28 12:38:05 +0100

    day 23 smaller board

--- test_day_23.py
+++ test_day_23.py
@@ -8,8 +8,8 @@ from utils import shift
 
 
 energies = {'A': 1, 'B': 10, 'C': 100, 'D': 1000}
-home_columns = {'A': 3, 'B': 5, 'C': 7, 'D': 9}
-forbidden = tuple(itertools.product((1,), (c for c in home_columns.values())))
+home_columns = {'A': 2, 'B': 4, 'C': 6, 'D': 8}
+forbidden = tuple(itertools.product((0,), (c for c in home_columns.values())))
 
 
 def get_data(input_file):
@@ -18,7 +18,7 @@ def get_data(input_file):
     for board in boards:
         lines = board.splitlines()
         result.append(np.array([list(line) + [' ']*(len(lines[0])-len(line)) for line in lines]))
-    return result
+    return [r[1:-1, 1:-1] for r in result]
 
 
 def get_data_part_one(input_file):
@@ -28,14 +28,14 @@ def get_data_part_one(input_file):
 
 def get_data_part_two(input_file):
     boards = get_data(input_file)
-    unfold = ("  #D#C#B#A#  ", "  #D#B#A#C#  ")
+    unfold = (" #D#C#B#A# ", " #D#B#A#C# ")
     result = []
     for board in boards:
-        if board.shape[0] == 5:
+        if board.shape[0] == 3:
             new_data = np.zeros_like(board, shape=(board.shape[0]+2, board.shape[1]))
-            new_data[0:3] = board[0:3]
-            new_data[3:5] = np.array([list(line) for line in unfold])
-            new_data[5:] = board[3:]
+            new_data[0:2] = board[0:2]
+            new_data[2:4] = np.array([list(line) for line in unfold])
+            new_data[4:] = board[4:]
         else:
             new_data = board
         result.append(new_data)
@@ -56,8 +56,8 @@ def one_step(start, board: np.array):
 
 def check_blocking(amphipod, board, z1):
     home1 = home_columns[amphipod]
-    for b in np.isin(board[1], ('A', 'B', 'C', 'D')).nonzero()[0]:
-        home2 = home_columns[board[1, b]]
+    for b in np.isin(board[0], ('A', 'B', 'C', 'D')).nonzero()[0]:
+        home2 = home_columns[board[0, b]]
         if home1 < b < z1 < home2 or home2 < z1 < b < home1:
             return True
     return False
@@ -66,12 +66,12 @@ def check_blocking(amphipod, board, z1):
 def legal_move(amphipod, board, x0, z0, z1):
     if (z0, z1) in forbidden:
         return False
-    if z0 == 1 and check_blocking(amphipod, board, z1):
+    if z0 == 0 and check_blocking(amphipod, board, z1):
         return False
-    homerun = z1 == home_columns[amphipod] and np.all(board[z0+1:-1, z1] == amphipod)
-    if x0 == 1:
+    homerun = z1 == home_columns[amphipod] and np.all(board[z0+1:, z1] == amphipod)
+    if x0 == 0:
         return homerun
-    return z0 == 1 or homerun
+    return z0 == 0 or homerun
 
 
 def find_next_moves(x0, x1, seen, board, energy):
@@ -102,7 +102,7 @@ def free_amphipods(board):
     amphipods = np.isin(board, ('A', 'B', 'C', 'D')).nonzero()
     index = np.zeros_like(amphipods[0], dtype=bool)
     for i, (ax0, ax1) in enumerate(zip(*amphipods)):
-        if ax0 == 1 or ax1 != home_columns[board[ax0, ax1]] or np.any(board[ax0+1:-1, ax1] != board[ax0, ax1]):
+        if ax0 == 0 or ax1 != home_columns[board[ax0, ax1]] or np.any(board[ax0+1:, ax1] != board[ax0, ax1]):
             index[i] = True
     return amphipods[0][index], amphipods[1][index]
 
@@ -113,7 +113,7 @@ def to_tuple(board):
 
 def is_winning(board):
     for key, value in home_columns.items():
-        if np.any(board[2:-1, value] != key):
+        if np.any(board[1:, value] != key):
             return False
     return True
 
@@ -138,7 +138,8 @@ def play(board: np.array):
 
 
 @pytest.mark.parametrize("input_file,get_data_fn,expected",
-                         (("input/day_23_example.txt", get_data_part_one, 12521),
+                         (("input/day_23_optimize.txt", get_data_part_one, 460),
+                          ("input/day_23_example.txt", get_data_part_one, 12521),
                           ("input/day_23.txt", get_data_part_one, 12530),
                           ("input/day_23_example.txt", get_data_part_two, 44169),
                           ("input/day_23.txt", get_data_part_two, 50492)))
