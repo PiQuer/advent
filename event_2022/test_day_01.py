59cc6ea31dfe53f07f45b524643308867c3af06c
Raimar Sandner <mail@raimar-sandner.de>
2022-12-02 22:24:46 +0100

    pytest-benchmark and linear solution for day 01

--- event_2022/test_day_01.py
+++ event_2022/test_day_01.py
@@ -3,24 +3,17 @@ from pathlib import Path
 import numpy as np
 
 
+FILE_NAMES = {'example': Path("input/day_01_example.txt"), 'real': Path("input/day_01.txt")}
+
+
 def _get_data(path: Path):
     current = 0
-    calories = []
     for line in path.read_text().split('\n'):
         if not line:
-            calories.append(current)
+            yield current
             current = 0
         else:
             current += int(line)
-    return calories
-
-
-@pytest.fixture(scope="module")
-def data():
-    return {
-        'example': _get_data(Path("input/day_01_example.txt")),
-        'real': _get_data(Path("input/day_01.txt"))
-    }
 
 
 par = pytest.mark.parametrize("data_set,expected,top",
@@ -28,26 +21,56 @@ par = pytest.mark.parametrize("data_set,expected,top",
                                ("example", 45000, 3), ("real", 196804, 3)))
 
 
+@pytest.mark.benchmark(warmup=True, warmup_iterations=100)
 class TestDay01:
-    @pytest.fixture(scope="class")
-    def data_sorted(self, data):
-        return {
-            'example': list(sorted(data['example'])),
-            'real': list(sorted(data['real']))
-        }
 
-    @pytest.fixture(scope="class")
-    def data_array(self, data):
-        return {
-            'example': np.array(data['example']),
-            'real': np.array(data['real'])
-        }
+    @staticmethod
+    def solution_with_sort(data_set, top):
+        return sum(sorted(_get_data(FILE_NAMES[data_set]))[-top:])
+
+    @staticmethod
+    def solution_with_array(data_set, top):
+        data = np.fromiter(_get_data(FILE_NAMES[data_set]), dtype=np.int32)
+        return np.sum(data[np.argpartition(data, -top)[-top:]])
+
+    @staticmethod
+    def solution_linear_1(data_set):
+        max_calories = 0
+        for calories in _get_data(FILE_NAMES[data_set]):
+            if calories > max_calories:
+                max_calories = calories
+        return max_calories
+
+    @staticmethod
+    def solution_linear_3(data_set):
+        max_0 = max_1 = max_2 = 0
+        for calories in _get_data(FILE_NAMES[data_set]):
+            if calories > max_2:
+                max_0 = max_1
+                max_1 = max_2
+                max_2 = calories
+            elif calories > max_1:
+                max_0 = max_1
+                max_1 = calories
+            elif calories > max_0:
+                max_0 = calories
+        return max_0 + max_1 + max_2
 
     @par
-    def test_with_sort(self, data_sorted, data_set, expected, top):
-        assert sum(data_sorted[data_set][-top:]) == expected
+    def test_with_sort(self, data_set, expected, top, benchmark):
+        result = benchmark(self.solution_with_sort, data_set, top)
+        assert result == expected
 
     @par
-    def test_with_array(self, data_array, data_set, expected, top):
-        data = data_array[data_set]
-        assert np.sum(data[np.argpartition(data, -top)[-top:]]) == expected
+    def test_with_array(self, data_set, expected, top, benchmark):
+        result = benchmark(self.solution_with_array, data_set, top)
+        assert result == expected
+
+    @par
+    def test_linear(self, data_set, expected, top, benchmark):
+        result = 0
+        if top == 3:
+            result = benchmark(self.solution_linear_3, data_set)
+        if top == 1:
+            result = benchmark(self.solution_linear_1, data_set)
+        assert result == expected
