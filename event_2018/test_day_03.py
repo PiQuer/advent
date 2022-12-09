b423fc23d01e8e4a42a023b6a2c786da0be2b91b
Raimar Sandner <mail@raimar-sandner.de>
2022-12-09 01:29:10 +0100

    2018 day 03

---
+++ event_2018/test_day_03.py
@@ -0,0 +1,33 @@
+import pytest
+import numpy as np
+import re
+from typing import Iterator
+
+from utils import dataset_parametrization, DataSetBase
+
+
+class DataSet(DataSetBase):
+    def claims(self) -> Iterator[tuple[int, int, int, int, int]]:
+        for claim in self.lines():
+            yield (int(g) for g in re.match(r"#(\d+) @ (\d+),(\d+): (\d+)x(\d+)", claim).groups())
+
+
+round_1 = dataset_parametrization("03", examples=[("", 4)], result=118322, dataset_class=DataSet)
+round_2 = dataset_parametrization("03", examples=[("", 3)], result=1178, dataset_class=DataSet)
+
+
+@pytest.mark.parametrize(**round_1)
+def test_round_1(dataset: DataSet):
+    patch = np.zeros((1000, 1000), dtype=np.int32)
+    for _, y, x, height, width in dataset.claims():
+        patch[y:y+height, x:x+width] += 1
+    assert np.sum(patch > 1) == dataset.result
+
+
+@pytest.mark.parametrize(**round_2)
+def test_round_2(dataset: DataSet):
+    patch = np.zeros((1000, 1000), dtype=np.int32)
+    for _, y, x, height, width in dataset.claims():
+        patch[y:y+height, x:x+width] += 1
+    assert next(p_id for p_id, y, x, height, width in dataset.claims() if np.all(patch[y:y+height, x:x+width] == 1)) \
+        == dataset.result
