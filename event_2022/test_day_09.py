0b996c203ba5ad498c4dceb0590ebfb8a74cb279
Raimar Sandner <mail@raimar-sandner.de>
2022-12-10 23:31:42 +0100

    2022 day 09 faster solution

--- event_2022/test_day_09.py
+++ event_2022/test_day_09.py
@@ -21,20 +21,20 @@ round_1 = dataset_parametrization(day="09", examples=[("1", 13)], result=5513, d
 round_2 = dataset_parametrization(day="09", examples=[("1", 1), ("2", 36)], result=2427, dataset_class=DataSet)
 
 
-def update_pos(head: np.array, tail: np.array):
-    lead = head - tail
-    if np.max(np.abs(lead)) > 1:
-        tail[:] += np.clip(lead, -1, 1)
+def update_pos(head: list[int], tail: list[int]):
+    lead = list(map(sub, head, tail))
+    if any(x > 1 for x in map(abs, lead)):
+        tail[:] = map(add, tail, map(lambda x: -1 if x < -1 else (1 if x > 1 else x), lead))
 
 
 class Day09:
     length = 0
 
     def test_puzzle(self, dataset: DataSet):
-        pos = [np.array((0, 0)) for _ in range(self.length)]
+        pos = [[0, 0] for _ in range(self.length)]
         seen = {tuple(pos[-1])}
         for d in dataset.directions():
-            pos[0][:] += d
+            pos[0][:] = map(add, pos[0], d)
             consume(starmap(update_pos, pairwise(pos)))
             seen.add(tuple(pos[-1]))
         assert len(seen) == dataset.result
