ee7080e214a209c24bb696f5fd867adcf8e144fc
Raimar Sandner <mail@raimar-sandner.de>
2022-12-11 21:59:34 +0100

    2020 day 05 data

---
+++ event_2020/test_day_05.py
@@ -0,0 +1,13 @@
+"""
+https://adventofcode.com/2020/day/5
+"""
+import pytest
+from utils import dataset_parametrization, DataSetBase
+
+
+round_1 = dataset_parametrization(day="05", examples=[("", 820)], result=None)
+
+
+@pytest.mark.parametrize(**round_1)
+def test_round_1(dataset: DataSetBase):
+    pass
