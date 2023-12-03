608d0ceb328b44af4ab9a7c4286b571723f540a4
Raimar Sandner <mail@raimar-sandner.de>
2023-12-03 23:14:58 +0100

    2023 day 03

---
+++ tests/event_2023/test_day_03.py
@@ -0,0 +1,76 @@
+import numpy as np
+import pytest
+from more_itertools import peekable
+import tinyarray as ta
+from collections import defaultdict
+
+from utils import dataset_parametrization, DataSetBase, inbounds
+
+round_1 = dataset_parametrization(year="2023", day="03", examples=[("", 4361)], result=544433)
+round_2 = dataset_parametrization(year="2023", day="03", examples=[("", 467835)], result=76314915)
+
+
+def is_symbol(a: np.array, idx: ta.array, symbol: bytes = None) -> bool:
+    if symbol is None:
+        return inbounds(ta.array(a.shape), idx) and (not a[*idx].isdigit()) and a[*idx] != b'.'
+    return inbounds(ta.array(a.shape), idx) and a[*idx] == symbol
+
+
+def get_number(a: np.array, index_iter: peekable) -> int:
+    idx = ta.array(next(index_iter))
+    if not a[*idx].isdigit():
+        return 0
+    result = 0
+    start = idx
+    use = any(is_symbol(a, start + ta.array(offset)) for offset in ((-1, -1), (0, -1), (1, -1)))
+    while a[*idx].isdigit():
+        result = 10 * result + int(a[*idx])
+        use = use or any(is_symbol(a, idx + ta.array(offset)) for offset in ((-1, 0), (1, 0)))
+        if idx[1] == a.shape[1] - 1:
+            return result if use else 0
+        idx = ta.array(next(index_iter))
+    use = use or any(is_symbol(a, idx + ta.array(offset)) for offset in ((-1, 0), (0, 0), (1, 0)))
+    return result if use else 0
+
+
+def update_gear(a: np.array, index_iter: peekable, gears: defaultdict):
+    idx = ta.array(next(index_iter))
+    if not a[*idx].isdigit():
+        return
+    number = 0
+    start = idx
+    stars = set()
+    def extend_stars(s: set, i):
+        s.update(set(i + ta.array(offset) for offset in ((-1, 0), (0, 0), (1, 0))
+                     if is_symbol(a, i + ta.array(offset), b'*')))
+    extend_stars(stars, start + ta.array((0, -1)))
+    while a[*idx].isdigit():
+        number = 10 * number + int(a[*idx])
+        extend_stars(stars, idx)
+        if idx[1] == a.shape[1] - 1:
+            break
+        idx = ta.array(next(index_iter))
+    extend_stars(stars, idx)
+    for star in stars:
+        gears[star].add(number)
+    return
+
+
+@pytest.mark.parametrize(**round_1)
+def test_round_1(dataset: DataSetBase):
+    result = 0
+    a = dataset.np_array_bytes()
+    index_iter = peekable(np.ndindex(a.shape))
+    while index_iter:
+        result += get_number(a, index_iter)
+    assert result == dataset.result
+
+
+@pytest.mark.parametrize(**round_2)
+def test_round_2(dataset: DataSetBase):
+    a = dataset.np_array_bytes()
+    index_iter = peekable(np.ndindex(a.shape))
+    gears = defaultdict(lambda: set())
+    while index_iter:
+        update_gear(a, index_iter, gears)
+    assert sum(np.prod(list(gears[k])) for k in gears.keys() if len(gears[k]) == 2) == dataset.result
