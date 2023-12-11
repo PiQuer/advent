8c326817cc93822b6ffe8fb646cda227f36061ca
Raimar Sandner <raimar.sandner@sick.de>
2023-12-11 12:56:52 +0100

    2023 day 10 part 2

--- tests/event_2023/test_day_10.py
+++ tests/event_2023/test_day_10.py
@@ -1,12 +1,16 @@
 import itertools
 from dataclasses import dataclass
 from functools import cached_property
+from itertools import takewhile
 
 import numpy as np
 import pytest
 import tinyarray as ta
+from more_itertools import iterate, one
 
 from utils import dataset_parametrization, DataSetBase, adjacent, inbounds
+
+rotation = ta.array([[0, 1], [-1, 0]])
 
 pipes = {
     b"|": {ta.array((-1, 0)), ta.array((1, 0))},
@@ -18,31 +22,29 @@ pipes = {
     b".": {},
 }
 
-outside = {
-    b"|": {ta.array((1, 0)): {ta.array((0, -1))}, ta.array((-1, 0)): {ta.array((0, 1))}},
-    b"-": {ta.array((0, 1)): {ta.array((1, 0))}, ta.array((0, -1)): {ta.array((-1, 0))}},
-    b"L": {ta.array((1, 0)): {ta.array((0, -1)), ta.array((1, 0))}, ta.array((0, -1)): {}},
-    b"J": {ta.array((1, 0)): {}, ta.array((0, 1)): {ta.array((1, 0)), ta.array((0, 1))}},
-    b"7": {ta.array((0, 1)): {}, ta.array((-1, 0)): {ta.array((0, 1)), ta.array((-1, 0))}},
-    b"F": {ta.array((0, -1)): {ta.array((-1, 0)), ta.array((0, -1))}, ta.array((-1, 0)): {}}
-}
 
 @dataclass
 class State:
     pos: ta.ndarray_int
     next_pos: ta.ndarray_int
-    field: np.ndarray
-    turn: int
+    prev_pos: ta.ndarray_int
 
-    def advance(self) -> "State":
-        try:
-            next_pos = self.next_pos + (pipes[self.field[*self.next_pos]] - {self.pos - self.next_pos}).pop()
-        except KeyError:
-            next_pos = None
-            turn = 0
-        else:
-            turn = cross_product(self.pos-self.next_pos, next_pos-self.next_pos)
-        return State(pos=self.next_pos, field=self.field, next_pos=next_pos, turn=turn)
+    @cached_property
+    def turn(self) -> int:
+        return cross_product((self.next_pos - self.pos), (self.prev_pos - self.pos))
+
+    @cached_property
+    def inside_neighbors(self) -> set["State"]:
+        v1 = self.prev_pos - self.pos
+        v2 = self.next_pos - self.pos
+        return set(self.pos + z for z in
+                   takewhile(lambda v: v != v2, iterate(lambda x: ta.dot(rotation, x), ta.dot(rotation, v1))))
+
+
+def advance(s: State, field: np.ndarray) -> "State":
+    next_pos = s.next_pos + one(pipes[field[*s.next_pos]] - {s.pos - s.next_pos})
+    return State(pos=s.next_pos, next_pos=next_pos, prev_pos=s.pos)
+
 
 class DataSet(DataSetBase):
     @cached_property
@@ -53,13 +55,13 @@ class DataSet(DataSetBase):
         start_index = ta.array(np.argwhere(self.np_array_bytes() == b'S')[0])
         connected = list(candidate for a in adjacent() if inbounds(self.field.shape, (candidate := start_index - a))
                          and a in pipes[self.field[*candidate]])
-        return State(pos=start_index, next_pos=connected[0], field=self.field,
-                     turn=cross_product(connected[1]-start_index, connected[0]-start_index)), \
-               State(pos=start_index, next_pos=connected[1], field=self.field,
-                     turn=cross_product(connected[0]-start_index, connected[1]-start_index)),
+        return State(pos=start_index, next_pos=connected[0], prev_pos=connected[1]), \
+               State(pos=start_index, next_pos=connected[1], prev_pos=connected[0])
 
 
 round_1 = dataset_parametrization(year="2023", day="10", examples=[("1", 4), ("2", 8)], result=6927,
+                                  dataset_class=DataSet)
+round_2 = dataset_parametrization(year="2023", day="10", examples=[("3", 4), ("4", 8), ("5", 10)], result=467,
                                   dataset_class=DataSet)
 
 
@@ -67,7 +69,7 @@ round_1 = dataset_parametrization(year="2023", day="10", examples=[("1", 4), ("2
 def test_round_1(dataset: DataSet):
     cursor1, cursor2 = dataset.starting_positions()
     for c in itertools.count(1):
-        cursor1, cursor2 = cursor1.advance(), cursor2.advance()
+        cursor1, cursor2 = advance(cursor1, dataset.field), advance(cursor2, dataset.field)
         if cursor1.pos == cursor2.pos:
             break
     else:
@@ -79,14 +81,28 @@ def cross_product(v1: ta.ndarray_int, v2: ta.ndarray_int):
     return v1[0]*v2[1] - v1[1]*v2[0]
 
 
-@pytest.mark.parametrize(**round_1)
+@pytest.mark.parametrize(**round_2)
 def test_round_2(dataset: DataSet):
     start1, start2 = dataset.starting_positions()
-    cursor = start1
-    turn = cursor.turn
+    cursor1, cursor2 = start1, start2
+    loop1, loop2 = {}, {}
+    turn1, turn2 = 0, 0
     while True:
-        cursor = cursor.advance()
-        turn += cursor.turn
-        if cursor.pos == start1.pos:
+        turn1 += cursor1.turn
+        turn2 += cursor2.turn
+        loop1[cursor1.pos] = cursor1
+        loop2[cursor2.pos] = cursor2
+        if cursor1.next_pos == start1.pos:
             break
-    start = start1 if turn == 4 else start2
+        cursor1 = advance(cursor1, dataset.field)
+        cursor2 = advance(cursor2, dataset.field)
+    loop = loop1 if turn1 == 4 else loop2
+    inside_tiles = set(t for l in loop.values() for t in l.inside_neighbors if t not in loop)
+    to_examine = inside_tiles.copy()
+    while to_examine:
+        next_tile = to_examine.pop()
+        for a in adjacent():
+            if (candidate := next_tile + a) not in inside_tiles and candidate not in loop:
+                inside_tiles.add(candidate)
+                to_examine.add(candidate)
+    assert len(inside_tiles) == dataset.result
