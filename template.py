e1be4949296bb3413bf7f9f64998bcaabc66cd14
Raimar Sandner <mail@raimar-sandner.de>
2023-12-12 18:55:23 +0100

    added empty input files and template

---
+++ template.py
@@ -0,0 +1,25 @@
+import pytest
+
+from utils import dataset_parametrization, DataSetBase
+
+# from utils import generate_rounds
+
+year="2023"
+day="00"
+
+class DataSet(DataSetBase):
+    pass
+
+round_1 = dataset_parametrization(year=year, day=day, examples=[("", None)], result=None, dataset_class=DataSet)
+round_2 = dataset_parametrization(year=year, day=day, examples=[("", None)], result=None, dataset_class=DataSet)
+# pytest_generate_tests = generate_rounds(round_1, round_2)
+
+
+@pytest.mark.parametrize(**round_1)
+def test_round_1(dataset):
+    pass
+
+
+@pytest.mark.parametrize(**round_2)
+def test_round_2(dataset):
+    pass
