54405581edcd9904708d53e9e29874e21b6377ab
Raimar Sandner <mail@raimar-sandner.de>
2023-12-16 16:26:04 +0100

    2023 day 16 round 2

--- adventofcode/event_2023/test_day_16.py
+++ adventofcode/event_2023/test_day_16.py
@@ -2,23 +2,22 @@
 --- Day 16: The Floor Will Be Lava ---
 https://adventofcode.com/2023/day/16
 """
-from collections import defaultdict, deque
+from collections import deque
 from dataclasses import dataclass
+from itertools import chain
 
 import numpy as np
 import pytest
 import tinyarray as ta
+from more_itertools import one
 
 from adventofcode.utils import dataset_parametrization, DataSetBase, inbounds
-
-# from utils import generate_rounds
 
 YEAR="2023"
 DAY= "16"
 
 round_1 = dataset_parametrization(year=YEAR, day=DAY, examples=[("", 46)], result=7728, dataset_class=DataSetBase)
-round_2 = dataset_parametrization(year=YEAR, day=DAY, examples=[("", None)], result=None, dataset_class=DataSetBase)
-# pytest_generate_tests = generate_rounds(round_1, round_2)
+round_2 = dataset_parametrization(year=YEAR, day=DAY, examples=[("", 51)], result=8061, dataset_class=DataSetBase)
 
 
 @dataclass(frozen=True)
@@ -26,41 +25,75 @@ class Beam:
     coordinates: ta.ndarray_int
     direction: ta.ndarray_int
 
+@dataclass
+class PropagationResult:
+    energized: set[ta.ndarray_int]
+    starting_points: set[Beam]
 
-PROPAGATION = {
-    b".": {},
-    b"-": {ta.array((1, 0)): {ta.array((0, -1)), ta.array((0, 1))},
-          ta.array((-1, 0)): {ta.array((0, -1)), ta.array((0, 1))}},
-    b"|": {ta.array((0, 1)): {ta.array((1, 0)), ta.array((-1, 0))},
-          ta.array((0, -1)): {ta.array((1, 0)), ta.array((-1, 0))}},
-    b"/": {ta.array((0, 1)): {ta.array((-1, 0))},
-          ta.array((0, -1)): {ta.array((1, 0))},
-          ta.array((1, 0)): {ta.array((0, -1))},
-          ta.array((-1, 0)): {ta.array((0, 1))}},
-    b"\\": {ta.array((0, 1)): {ta.array((1, 0))},
-            ta.array((0, -1)): {ta.array((-1, 0))},
-            ta.array((1, 0)): {ta.array((0, 1))},
-            ta.array((-1, 0)): {ta.array((0, -1))}}
+class BeamDeflection(dict):
+    def __missing__(self, key):
+        return {key}
+
+DEFLECTIONS = {
+    b".": BeamDeflection(),
+    b"-": BeamDeflection({ta.array((1, 0)): {ta.array((0, -1)), ta.array((0, 1))},
+                          ta.array((-1, 0)): {ta.array((0, -1)), ta.array((0, 1))}}),
+    b"|": BeamDeflection({ta.array((0, 1)): {ta.array((1, 0)), ta.array((-1, 0))},
+                          ta.array((0, -1)): {ta.array((1, 0)), ta.array((-1, 0))}}),
+    b"/": BeamDeflection({ta.array((0, 1)): {ta.array((-1, 0))},
+                          ta.array((0, -1)): {ta.array((1, 0))},
+                          ta.array((1, 0)): {ta.array((0, -1))},
+                          ta.array((-1, 0)): {ta.array((0, 1))}}),
+    b"\\": BeamDeflection({ta.array((0, 1)): {ta.array((1, 0))},
+                           ta.array((0, -1)): {ta.array((-1, 0))},
+                           ta.array((1, 0)): {ta.array((0, 1))},
+                           ta.array((-1, 0)): {ta.array((0, -1))}})
 }
 
-def propagate(start: Beam, contraption: np.ndarray) -> defaultdict[ta.ndarray_int, set[Beam]]:
-    result = defaultdict(set)
-    result[ta.array((0, 0))].add(start)
-    beams = deque((start,))
-    while beams:
-        beam = beams.pop()
-        for next_step in PROPAGATION[contraption[*beam.coordinates]].get(beam.direction, {beam.direction}):
-            if inbounds(contraption.shape, next_coordinates := beam.coordinates + next_step):
-                if (next_beam := Beam(next_coordinates, next_step)) not in result[next_coordinates]:
-                    beams.append(next_beam)
-                    result[next_coordinates].add(next_beam)
+def linear_propagation(start: Beam, contraption: np.ndarray,
+                       cache: dict[Beam, PropagationResult]) -> PropagationResult:
+    if start in cache:
+        return cache[start]
+    beams = set()
+    current = start
+    next_beams = set()
+    while inbounds(contraption.shape, current.coordinates) and current not in beams:
+        next_directions = DEFLECTIONS[contraption[*current.coordinates]][current.direction]
+        if len(next_directions) == 2:
+            next_beams.update(Beam(current.coordinates, d) for d in next_directions)
+            break
+        beams.add(current)
+        current = Beam(current.coordinates + one(next_directions), one(next_directions))
+    result = PropagationResult({c.coordinates for c in beams}, next_beams)
+    cache[start] = result
     return result
+
+
+def propagate(start: Beam, contraption: np.ndarray, cache: dict[Beam, PropagationResult]) -> int:
+    visited_splitters: set[Beam] = set()
+    starting_points: deque[Beam] = deque((start,))
+    energized_tiles: set[ta.ndarray_int] = set()
+    while starting_points:
+        starting_point = starting_points.pop()
+        visited_splitters.add(starting_point)
+        propagation_result = linear_propagation(starting_point, contraption, cache)
+        energized_tiles.update(propagation_result.energized)
+        starting_points.extend(s for s in propagation_result.starting_points if not s in visited_splitters)
+    return len(energized_tiles)
 
 @pytest.mark.parametrize(**round_1)
 def test_round_1(dataset: DataSetBase):
-    assert len(propagate(Beam(ta.array((0, 0)), ta.array((0, 1))), dataset.np_array_bytes())) == dataset.result
+    cache = {}
+    assert propagate(Beam(ta.array((0, 0)), ta.array((0, 1))), dataset.np_array_bytes(), cache) == dataset.result
 
 
 @pytest.mark.parametrize(**round_2)
 def test_round_2(dataset: DataSetBase):
-    pass
+    cache = {}
+    contraption = dataset.np_array_bytes()
+    starting_positions = \
+        ((Beam(ta.array((p, 0)), ta.array((0, 1))) for p in range(contraption.shape[0])),
+         (Beam(ta.array((p, contraption.shape[1]-1)), ta.array((0, -1))) for p in range(contraption.shape[0])),
+         (Beam(ta.array((0, p)), ta.array((1, 0))) for p in range(contraption.shape[1])),
+         (Beam(ta.array((contraption.shape[0]-1, p)), ta.array((-1, 0))) for p in range(contraption.shape[1])))
+    assert max(propagate(s, contraption, cache) for s in chain.from_iterable(starting_positions)) == dataset.result
