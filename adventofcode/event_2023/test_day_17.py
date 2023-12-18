a69798da5b1f6729f13bb58a042c5e33ef7eb71d
Raimar Sandner <raimar.sandner@sick.de>
2023-12-18 12:15:54 +0100

    2023 day 17 round 1

--- adventofcode/event_2023/test_day_17.py
+++ adventofcode/event_2023/test_day_17.py
@@ -2,14 +2,18 @@
 --- Day 17: Clumsy Crucible ---
 https://adventofcode.com/2023/day/17
 """
+import heapq
+import math
+from collections import defaultdict
 from dataclasses import dataclass
+from typing import Optional
 
 import numpy as np
 import pytest
 import tinyarray as ta
+from more_itertools import only, one
 
-from adventofcode.utils import dataset_parametrization, DataSetBase
-from utils import Waypoint, adjacent, inbounds
+from utils import adjacent, inbounds, dataset_parametrization, DataSetBase
 
 # from utils import generate_rounds
 
@@ -19,7 +23,7 @@ DAY= "17"
 class DataSet(DataSetBase):
     pass
 
-round_1 = dataset_parametrization(year=YEAR, day=DAY, examples=[("", 102)], result=None, dataset_class=DataSet)
+round_1 = dataset_parametrization(year=YEAR, day=DAY, examples=[("", 102)], result=870, dataset_class=DataSet)
 round_2 = dataset_parametrization(year=YEAR, day=DAY, examples=[("", None)], result=None, dataset_class=DataSet)
 # pytest_generate_tests = generate_rounds(round_1, round_2)
 
@@ -28,6 +32,7 @@ round_2 = dataset_parametrization(year=YEAR, day=DAY, examples=[("", None)], res
 class PathCoordinates:
     coordinates: ta.ndarray_int
     heading: ta.ndarray_int
+    distance: int = math.inf
 
 @dataclass(frozen=True)
 class PathState:
@@ -35,36 +40,86 @@ class PathState:
     straight_segments: int
 
     def __lt__(self, other: "PathState"):
-        return self.value < other.value and self.straight_segments <= other.straight_segments
+        return (self.value < other.value and self.straight_segments <= other.straight_segments) \
+            or (self.value == other.value) and self.straight_segments < other.straight_segments
+
+    def __eq__(self, other: "PathState"):
+        return self.value == other.value and self.straight_segments == other.straight_segments
 
 @dataclass(frozen=True)
 class CrucibleWaypoint:
-    length: int
-    previous: PathCoordinates | None
+    previous: Optional["CrucibleWaypoint"]
     state: PathState
+    this: PathCoordinates
 
-def get_best_paths(length: int, best_paths: dict[ta.ndarray_int, set[CrucibleWaypoint]], data: np.ndarray,
-                   candidates: set[PathCoordinates]) -> set[PathCoordinates]:
-    next_candidates: set[PathCoordinates] = set()
-    for candidate in candidates:
-        neighbors = (PathCoordinates(next_coordinates, h) for h in adjacent()
-                     if inbounds(data.shape, next_coordinates := candidate.coordinates + h))
-        for neighbor in neighbors:
-            pass
-    return next_candidates
+    def __lt__(self, other: "CrucibleWaypoint"):
+        return self.this.distance < other.this.distance or \
+            (self.this.distance == other.this.distance and self.state.value < other.state.value)
+
+    def __str__(self):
+        previous_coordinates = self.previous.this.coordinates if self.previous is not None else None
+        return f"Previous: {previous_coordinates}, This: {self.this.coordinates}, " \
+               f"Heading: {self.this.heading}, Straight: {self.state.straight_segments}, Value: {self.state.value})"
+
+    def __repr__(self):
+        return self.__str__()
+
+def visualize_path(data: np.ndarray, waypoint: CrucibleWaypoint):
+    visualize = data.copy()
+    while waypoint is not None:
+        visualize[*waypoint.this.coordinates] += 100
+        waypoint = waypoint.previous
+    return visualize
+
+
+def get_best_paths(best_paths: defaultdict[PathCoordinates, set[CrucibleWaypoint]], data: np.ndarray,
+                   candidates: list[CrucibleWaypoint], target_coordinates: PathCoordinates):
+    target_waypoint = only(best_paths[target_coordinates])
+    candidate = heapq.heappop(candidates)
+    neighbors = (PathCoordinates(next_coordinates, ta.array(h))
+                 for h in adjacent() if ta.array(h) != -candidate.this.heading and
+                 inbounds(data.shape, next_coordinates := candidate.this.coordinates + h))
+    for neighbor in neighbors:
+        next_straight_segments = 1 if candidate.this.heading != neighbor.heading else \
+            candidate.state.straight_segments + 1
+        if next_straight_segments > 3:
+            continue
+        if neighbor.coordinates == target_coordinates.coordinates:
+            next_straight_segments = 0
+        next_waypoint = \
+            CrucibleWaypoint(previous=candidate,
+                             state=PathState(value=candidate.state.value + data[*neighbor.coordinates],
+                                             straight_segments=next_straight_segments),
+                             this=neighbor)
+        if target_waypoint is not None and \
+                target_waypoint.state.value <= \
+                next_waypoint.state.value + sum(ta.abs(target_coordinates.coordinates - next_waypoint.this.coordinates)):
+            continue
+        to_remove = set()
+        competing = best_paths[neighbor if neighbor.coordinates != target_coordinates.coordinates else target_coordinates]
+        for c in competing:
+            if next_waypoint.state < c.state:
+                to_remove.add(c)
+            elif c.state < next_waypoint.state or c.state == next_waypoint.state:
+                break
+        else:
+            competing -= to_remove
+            competing.add(next_waypoint)
+            heapq.heappush(candidates, next_waypoint)
 
 @pytest.mark.parametrize(**round_1)
 def test_round_1(dataset: DataSet):
-    data = dataset.np_array()
-    initial_coordinates = PathCoordinates(heading=ta.ndarray_int((0, 0)), coordinates=ta.ndarray_int((0, 0)))
+    data = dataset.np_array_digits()
+    initial_coordinates = PathCoordinates(heading=ta.array((0, 0)), coordinates=ta.array((0, 0)))
+    target_coordinates = PathCoordinates(heading=ta.array((0, 0)), coordinates=ta.array(data.shape) - (1, 1))
     initial_state = PathState(value=0, straight_segments=0)
-    best_paths = {initial_coordinates: {CrucibleWaypoint(length=0, previous=None, state=initial_state)}}
-    length = 0
-    candidates = {ta.array((0, 0))}
+    best_paths: defaultdict[PathCoordinates, set[CrucibleWaypoint]] = defaultdict(set)
+    best_paths[initial_coordinates] = {CrucibleWaypoint(previous=None, state=initial_state,
+                                                        this=initial_coordinates)}
+    candidates = list(best_paths[initial_coordinates])
     while candidates:
-        length += 1
-        candidates = get_best_paths(length, best_paths, data, candidates)
-    assert best_paths[ta.array(data.shape) - (1, 1)].value == dataset.result
+        get_best_paths(best_paths, data, candidates, target_coordinates)
+    assert one(best_paths[target_coordinates]).state.value == dataset.result
 
 
 @pytest.mark.parametrize(**round_2)
