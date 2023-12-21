db19a0506d1a6301068125d3ce33ffdba0d500fb
Raimar Sandner <mail@raimar-sandner.de>
2023-12-21 19:56:31 +0100

    2023 day 18 round 2

--- adventofcode/event_2023/test_day_18.py
+++ adventofcode/event_2023/test_day_18.py
@@ -1,7 +1,8 @@
 """
---- Day XX: XXXXX ---
-https://adventofcode.com/2023/day/XX
+--- Day 18: Lavaduct Lagoon ---
+https://adventofcode.com/2023/day/18
 """
+from abc import ABCMeta, abstractmethod
 from dataclasses import dataclass
 
 import pytest
@@ -9,8 +10,6 @@ import tinyarray as ta
 from more_itertools import peekable
 
 from adventofcode.utils import dataset_parametrization, DataSetBase, cross_product
-
-# from adventofcode.utils import generate_rounds
 
 YEAR= "2023"
 DAY= "18"
@@ -40,51 +39,51 @@ class Loop:
     def __init__(self, segments: list[Segment]):
         self._segments = segments
         self._orientation = -1 if sum(cross_product(s.incoming_heading, s.outgoing_heading) for s in segments) < 0 \
-            else -1
+            else 1
 
     def area(self):
         return int(sum(s.area(self._orientation) for s in self._segments))
 
-class DataSet(DataSetBase):
+
+class DataSetDay18(DataSetBase, metaclass=ABCMeta):
+    @abstractmethod
+    def line_to_params(self, line:str) -> tuple[ta.ndarray_int, int]:
+        pass
+
     def loop(self) -> Loop:
         result: list[Segment] = []
-        lines = peekable(line.split() for line in self.lines())
+        lines = peekable(self.line_to_params(line) for line in self.lines())
         start = ta.array((0, 0))
-        for direction, length, _ in lines:
+        for incoming_heading, length in lines:
             next_line = lines.peek(None)
-            incoming_heading = HEADINGS[direction]
-            outgoing_heading = result[0].incoming_heading if next_line is None else HEADINGS[next_line[0]]
-            stop = start + int(length) * incoming_heading
+            outgoing_heading = result[0].incoming_heading if next_line is None else next_line[0]
+            stop = start + length * incoming_heading
             result.append(Segment(start=start, stop=stop, incoming_heading=incoming_heading,
                                   outgoing_heading=outgoing_heading))
             start = stop
         return Loop(result)
 
-round_1 = dataset_parametrization(year=YEAR, day=DAY, examples=[("", 62)], result=62500, dataset_class=DataSet)
-round_2 = dataset_parametrization(year=YEAR, day=DAY, examples=[("", None)], result=None, dataset_class=DataSet)
-# pytest_generate_tests = generate_rounds(round_1, round_2)
+class DataSetRound1(DataSetDay18):
+    def line_to_params(self, line:str) -> tuple[ta.ndarray_int, int]:
+        direction, length, _ = line.split()
+        return HEADINGS[direction], int(length)
 
-@pytest.mark.parametrize(
-    "segment, expected",
-    ((Segment(start=ta.array((0, 0)), stop=ta.array((1, 0)),
-              incoming_heading=ta.array((1, 0)), outgoing_heading=ta.array((0, 1))), 0.75),
-     (Segment(start=ta.array((1, 0)), stop=ta.array((1, 1)),
-              incoming_heading=ta.array((0, 1)), outgoing_heading=ta.array((-1, 0))), 0.75),
-     (Segment(start=ta.array((1, 1)), stop=ta.array((0, 1)),
-              incoming_heading=ta.array((-1, 0)), outgoing_heading=ta.array((0, -1))), 1.75),
-     (Segment(start=ta.array((0, 1)), stop=ta.array((0, 0)),
-              incoming_heading=ta.array((0, -1)), outgoing_heading=ta.array((1, 0))), 0.75))
-)
-def test_area(segment: Segment, expected: float):
-    assert segment.area() == expected
+class DataSetRound2(DataSetDay18):
+    def line_to_params(self, line:str) -> tuple[ta.ndarray_int, int]:
+        directive = line.split()[2]
+        return list(HEADINGS.values())[int(directive[-2:-1])], int(directive[2:-2], 16)
 
+round_1 = dataset_parametrization(year=YEAR, day=DAY, examples=[("", 62)], result=62500, dataset_class=DataSetRound1)
+round_2 = dataset_parametrization(year=YEAR, day=DAY, examples=[("", 952408144115)], result=122109860712709,
+                                  dataset_class=DataSetRound2)
 
 @pytest.mark.parametrize(**round_1)
-def test_round_1(dataset: DataSet):
+def test_round_1(dataset: DataSetRound1):
     loop = dataset.loop()
     assert loop.area() == dataset.result
 
 
 @pytest.mark.parametrize(**round_2)
-def test_round_2(dataset: DataSet):
-    assert dataset.result is None
+def test_round_2(dataset: DataSetRound2):
+    loop = dataset.loop()
+    assert loop.area() == dataset.result
