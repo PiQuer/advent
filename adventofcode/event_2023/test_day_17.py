133c5825dab19116b9622a7f696b184b6528ec22
Raimar Sandner <mail@raimar-sandner.de>
2023-12-21 15:12:28 +0100

    2023 day 17 round 2

--- adventofcode/event_2023/test_day_17.py
+++ adventofcode/event_2023/test_day_17.py
@@ -4,24 +4,20 @@ https://adventofcode.com/2023/day/17
 """
 import heapq
 import logging
-import math
 from collections import defaultdict
 from dataclasses import dataclass, field
 from typing import Optional
 
 import numpy as np
-import pytest
 import tinyarray as ta
-from more_itertools import only, one
+from more_itertools import one
 from ratelimitingfilter import RateLimitingFilter
 
-from adventofcode.utils import adjacent, inbounds, dataset_parametrization, DataSetBase
+from adventofcode.utils import adjacent, inbounds, dataset_parametrization, DataSetBase, generate_rounds
 
 ratelimit = RateLimitingFilter(rate=1, per=2, match=["Candidate"])
 logging.root.addFilter(ratelimit)
 
-
-# from utils import generate_rounds
 
 YEAR= "2023"
 DAY= "17"
@@ -29,10 +25,11 @@ DAY= "17"
 class DataSet(DataSetBase):
     pass
 
-round_1 = dataset_parametrization(year=YEAR, day=DAY, examples=[("", 102)], result=870, dataset_class=DataSet)
-round_2 = dataset_parametrization(year=YEAR, day=DAY, examples=[("", None)], result=None, dataset_class=DataSet)
-# pytest_generate_tests = generate_rounds(round_1, round_2)
-
+round_1 = dataset_parametrization(year=YEAR, day=DAY, examples=[("", 102)], result=870, dataset_class=DataSet,
+                                  initial_straight=1, max_straight=3)
+round_2 = dataset_parametrization(year=YEAR, day=DAY, examples=[("", 94)], result=1063, dataset_class=DataSet,
+                                  initial_straight=4, max_straight=10)
+pytest_generate_tests = generate_rounds(round_1, round_2)
 
 @dataclass(frozen=True)
 class PathCoordinates:
@@ -77,27 +74,35 @@ def visualize_path(data: np.ndarray, waypoint: CrucibleWaypoint):
 
 
 def get_best_paths(best_paths: defaultdict[PathCoordinates, set[CrucibleWaypoint]], data: np.ndarray,
-                   candidates: list[CrucibleWaypoint], target_coordinates: PathCoordinates):
+                   candidates: list[CrucibleWaypoint], target_coordinates: PathCoordinates,
+                   initial_straight: int, max_straight: int):
     candidate = heapq.heappop(candidates)
     logging.debug("Candidates: %s, current: %s", len(candidates), candidate)
-    neighbors = (PathCoordinates(next_coordinates, ta.array(h))
-                 for h in adjacent() if ta.array(h) != -candidate.this.heading and
-                 inbounds(data.shape, next_coordinates := candidate.this.coordinates + h))
-    for neighbor in neighbors:
-        next_straight_segments = 1 if candidate.this.heading != neighbor.heading else \
-            candidate.state.straight_segments + 1
-        if next_straight_segments > 3:
-            continue
-        if neighbor.coordinates == target_coordinates.coordinates:
+    for h in (ah for _ in adjacent() if (ah := ta.array(_)) != -candidate.this.heading):
+        if candidate.this.heading == h:
+            next_coordinates = candidate.this.coordinates + h
+            next_straight_segments = candidate.state.straight_segments + 1
+            if (not inbounds(data.shape, next_coordinates)) or next_straight_segments > max_straight:
+                continue
+            next_value = candidate.state.value + data[*(candidate.this.coordinates + h)]
+        else:
+            next_coordinates = candidate.this.coordinates + initial_straight * h
+            next_straight_segments = initial_straight
+            if not inbounds(data.shape, next_coordinates):
+                continue
+            next_value = candidate.state.value + \
+                         sum(data[*(candidate.this.coordinates + (r+1) * h)] for r in range(initial_straight))
+        if next_coordinates == target_coordinates.coordinates:
             next_straight_segments = 0
-            neighbor = PathCoordinates(neighbor.coordinates, ta.array((0, 0)))
+            neighbor = PathCoordinates(target_coordinates.coordinates, ta.array((0, 0)))
+        else:
+            neighbor = PathCoordinates(next_coordinates, h)
         next_waypoint = \
             CrucibleWaypoint(previous=candidate,
-                             state=PathState(value=candidate.state.value + data[*neighbor.coordinates],
-                                             straight_segments=next_straight_segments),
+                             state=PathState(value=next_value, straight_segments=next_straight_segments),
                              this=neighbor)
         to_remove = set()
-        competing = best_paths[neighbor if neighbor.coordinates != target_coordinates.coordinates else target_coordinates]
+        competing = best_paths[neighbor]
         for c in competing:
             if next_waypoint.state < c.state:
                 to_remove.add(c)
@@ -108,8 +113,7 @@ def get_best_paths(best_paths: defaultdict[PathCoordinates, set[CrucibleWaypoint
             competing.add(next_waypoint)
             heapq.heappush(candidates, next_waypoint)
 
-@pytest.mark.parametrize(**round_1)
-def test_round_1(dataset: DataSet):
+def test_day_17(dataset: DataSet):
     data = dataset.np_array_digits()
     initial_coordinates = PathCoordinates(heading=ta.array((0, 0)), coordinates=ta.array((0, 0)))
     target_coordinates = PathCoordinates(heading=ta.array((0, 0)), coordinates=ta.array(data.shape) - (1, 1))
@@ -119,12 +123,7 @@ def test_round_1(dataset: DataSet):
     best_paths[initial_coordinates] = {initial_waypoint}
     candidates = [initial_waypoint]
     while candidates:
-        get_best_paths(best_paths, data, candidates, target_coordinates)
+        get_best_paths(best_paths, data, candidates, target_coordinates, **dataset.params)
     result = one(best_paths[target_coordinates])
     logging.info("Result: %s", result)
     assert result.state.value == dataset.result
-
-
-@pytest.mark.parametrize(**round_2)
-def test_round_2(dataset: DataSet):
-    assert dataset.result is None
