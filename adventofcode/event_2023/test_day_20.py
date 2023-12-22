6cd2b73ce7af38b7e57a6f4d0863ce767a01a073
Raimar Sandner <mail@raimar-sandner.de>
2023-12-22 13:01:42 +0100

    2023 day 20 round 2

--- adventofcode/event_2023/test_day_20.py
+++ adventofcode/event_2023/test_day_20.py
@@ -3,16 +3,18 @@
 https://adventofcode.com/2023/day/20
 """
 import abc
+import math
 import operator
+import warnings
 from collections import deque
 from dataclasses import dataclass
+from functools import partial
 
 import pytest
 import tinyarray as ta
+from more_itertools import only
 
 from adventofcode.utils import dataset_parametrization, DataSetBase
-
-# from adventofcode.utils import generate_rounds
 
 YEAR= "2023"
 DAY= "20"
@@ -36,9 +38,6 @@ class Module(metaclass=abc.ABCMeta):
     def generate_pulses(self, high: bool) -> list[Pulse]:
         return [Pulse(source=self.name, destination=d, high=high) for d in self.destinations]
 
-    def __hash__(self):
-        return hash((self.name, self.destinations))
-
 class Broadcaster(Module):
     def __init__(self, destinations: tuple[str, ...]):
         super().__init__(name="broadcaster", destinations=destinations)
@@ -57,34 +56,28 @@ class Button(Module):
 class FlipFlop(Module):
     def __init__(self, name: str, destinations: tuple[str, ...]):
         super().__init__(name=name, destinations=destinations)
-        self._state = False
+        self.state = False
 
     def process(self, pulse: Pulse) -> list[Pulse]:
-        if not pulse.high:
-            self._state = not self._state
-            return self.generate_pulses(self._state)
-        return []
-
-    def __hash__(self):
-        return hash((super().__hash__(), self._state))
-
+        if pulse.high:
+            return []
+        self.state = not self.state
+        return self.generate_pulses(self.state)
 
 class Conjunction(Module):
     def __init__(self, name: str, destinations: tuple[str, ...]):
         super().__init__(name=name, destinations=destinations)
-        self._inputs = {}
+        self.inputs = {}
 
     def register_input(self, source: str):
-        self._inputs[source] = False
+        self.inputs[source] = False
 
     def process(self, pulse: Pulse) -> list[Pulse]:
-        self._inputs[pulse.source] = pulse.high
-        if all(i for i in self._inputs.values()):
+        self.inputs[pulse.source] = pulse.high
+        if all(i for i in self.inputs.values()):
             return self.generate_pulses(False)
         return self.generate_pulses(True)
 
-    def __hash__(self):
-        return hash((super().__hash__(), tuple(self._inputs.values())))
 
 class DataSet(DataSetBase):
     def modules(self) -> dict[str, Module]:
@@ -105,8 +98,7 @@ class DataSet(DataSetBase):
 
 round_1 = dataset_parametrization(year=YEAR, day=DAY, examples=[("1", 32000000), ("2", 11687500)], result=873301506,
                                   dataset_class=DataSet)
-round_2 = dataset_parametrization(year=YEAR, day=DAY, examples=[("", None)], result=None, dataset_class=DataSet)
-# pytest_generate_tests = generate_rounds(round_1, round_2)
+round_2 = dataset_parametrization(year=YEAR, day=DAY, examples=[], result=241823802412393, dataset_class=DataSet)
 
 
 def push_the_button(modules: dict[str, Module]) -> ta.ndarray_int:
@@ -122,25 +114,55 @@ def push_the_button(modules: dict[str, Module]) -> ta.ndarray_int:
 
 @pytest.mark.parametrize(**round_1)
 def test_round_1(dataset: DataSet):
+    target = 1000
     modules = dataset.modules()
     pulse_count = []
-    cache = {}
-    for n in range(1000):
-        key = tuple(modules.values())
-        if key in cache:
-            break
-        cache[key] = n
+    for _ in range(target):
         pulse_count.append(push_the_button(modules))
-    if n != 999:
-        first_index = cache[key]
-        cycle_length = n - first_index
-        cycles, rest = divmod(1000 - first_index, cycle_length)
-        result = sum(pulse_count[:first_index]) + sum(pulse_count[first_index:]) * cycles + \
-                 sum(pulse_count[first_index:first_index+rest])
-    else:
-        result = sum(pulse_count)
-    assert operator.mul(*result) == dataset.result
+    assert operator.mul(*sum(pulse_count)) == dataset.result
+
+
+def create_graph(modules: dict[str, Module], pulse: Pulse):
+    try:
+        # pylint: disable=import-outside-toplevel
+        from pyvis.network import Network
+    except ImportError:
+        warnings.warn("pyvis not installed")
+        return
+    net = Network(notebook=True, directed=True, height="800px")
+    net.add_node(n_id="rx", label="rx", color = "#C1C1C1", shape="circle")
+    for m in modules.values():
+        match str(m)[0]:
+            case "C":
+                m: Conjunction
+                color = "#FF9494"if all(i for i in m.inputs) else "#FF1B1B"
+            case "F":
+                m: FlipFlop
+                color = "#2164FF"if m.state else "#94B4FF"
+            case _:
+                color = "#C1C1C1"
+        net.add_node(n_id=m.name, label=m.name, color=color, shape="circle")
+    for m in modules.values():
+        for d in m.destinations:
+            if m.name == pulse.source and d == pulse.destination:
+                value = 2
+                title = "high" if pulse.high else "low"
+                net.add_edge(source=m.name, to=d, value=value, title=title, arrowStrikethrough=False)
+            else:
+                net.add_edge(source=m.name, to=d)
+    net.show("2023_day_20.html")
+
+
+def get_prime(b_str: str, modules: dict[str, Module]):
+    b = modules[b_str]
+    result = ""
+    while b is not None:
+        result = f"1{result}" if any(isinstance(modules[d], Conjunction) for d in b.destinations) else f"0{result}"
+        b = only(m for d in b.destinations if isinstance(m := modules[d], FlipFlop))
+    return int(result, 2)
+
 
 @pytest.mark.parametrize(**round_2)
 def test_round_2(dataset: DataSet):
-    assert dataset.result is None
+    modules = dataset.modules()
+    assert math.lcm(*map(partial(get_prime, modules=modules), modules['broadcaster'].destinations)) == dataset.result
