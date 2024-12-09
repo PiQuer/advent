b94072cae2251e166fd374e941be275bb1905000
Raimar Sandner <mail@raimar-sandner.de>
2024-12-09 20:42:05 +0100

    day 06 improvements

--- adventofcode/event_2024/test_day_06.py
+++ adventofcode/event_2024/test_day_06.py
@@ -45,28 +45,48 @@ def get_edge(position: ta.ndarray_int, orientation: int, m: np.array) -> tuple[t
 
 def update_edge(m: np.array, obstacle: ta.ndarray_int, orientation: int, graph: nx.DiGraph):
     if obstacle[1] == 0 or obstacle[0] == m.shape[0]-1:
-        return
+        [], []
     trajectory = m[obstacle[0]+1:, obstacle[1]-1]
     obstacles = np.argwhere(trajectory == b'#').reshape(-1)
+    deactivated_edges = []
+    added_edges = []
+    added_nodes = []
     if len(obstacles):
         start_node = ta.array((obstacle[0]+1+obstacles[0], obstacle[1]-1)), orientation
         end_node = (rot90(obstacle, m.shape), (orientation+1)%4)
         if (out_edge := first(graph.out_edges(start_node), default=None)):
             graph.remove_edge(*out_edge)
+            deactivated_edges.append(out_edge)
+        if graph.nodes.get(end_node) is None:
+            added_nodes.append(end_node)
         graph.add_edge(start_node, end_node)
+        added_edges.append((start_node, end_node))
+    return added_nodes, added_edges, deactivated_edges
 
 
 def add_edges(m: np.array, obstacle: ta.ndarray_int, graph: nx.DiGraph, update: bool = False):
     t = m
     start_node = ta.array(obstacle)
+    added_nodes = []
+    added_edges = []
+    deactivated_edges = []
     for orientation in range(4):
-        graph.add_node((start_node, orientation))
+        node = (start_node, orientation)
+        if graph.nodes.get(node) is None:
+            graph.add_node(node)
+            added_nodes.append(node)
         if (end_node := get_edge(start_node, orientation, t)) is not None:
-            graph.add_edge((start_node, orientation), end_node)
+            if graph.edges.get((node, end_node)) is None:
+                graph.add_edge(node, end_node)
+                added_edges.append((node, end_node))
         if update:
-            update_edge(t, start_node, orientation, graph)
+            added, updated, deactivated = update_edge(t, *node, graph)
+            added_nodes.extend(added)
+            added_edges.extend(updated)
+            deactivated_edges.extend(deactivated)
         start_node = rot90(start_node, t.shape)
         t = np.rot90(t)
+    return added_nodes, added_edges, deactivated_edges
 
 
 def rot90(position: ta.ndarray_int, shape: tuple, k: int = 1) -> ta.ndarray_int:
@@ -105,17 +125,39 @@ def test_part_1(dataset: DataSet):
 def test_part_2(dataset: DataSet):
     m = dataset.np_array_bytes
     graph, start_node = dataset.graph()
+    first_edge = first(graph.out_edges(start_node))
+    first_obstacle = rot90(first_edge[1][0], m.shape, k=-1)
     get_route(m, start_node, graph)
     m[*(start_node[0] + (0, 1))] = b'.'
     possible_locations = np.argwhere(m == b'X')
     result = 0
-    for location in possible_locations[:100]:
-        new_graph = graph.copy()
-        add_edges(m, ta.array(location), new_graph, update=True)
+    for location in possible_locations:
+        location = ta.array(location)
+        added_nodes, added_edges, deactivated_edges = add_edges(m, location, graph, update=True)
+        if location[1] == start_node[0][1] + 1 and first_obstacle[0] < location[0]:
+            graph.remove_edge(*first_edge)
+            graph.add_edge(* (new_edge := (start_node, (rot90(location, m.shape), 1))))
+            deactivated_edges.append(first_edge)
+            added_edges.append(new_edge)
         try:
-            nx.find_cycle(new_graph, start_node)
+            nx.find_cycle(graph, start_node)
         except nx.NetworkXNoCycle:
             continue
         else:
             result += 1
-    dataset.assert_answer(result, dryrun=True)
+        finally:
+            graph.remove_edges_from(added_edges)
+            graph.remove_nodes_from(added_nodes)
+            graph.add_edges_from(deactivated_edges)
+
+    dataset.assert_answer(result)
+
+
+def visualize(graph, filter=None):
+    from pyvis.network import Network
+    nt = Network('500px', '500px')
+    if filter is None:
+        nt.from_nx(nx.relabel_nodes(graph, str))
+    else:
+        nt.from_nx(nx.relabel_nodes(nx.subgraph_view(graph, filter_edge=filter), str))
+    nt.show("nx.html", notebook=False)
